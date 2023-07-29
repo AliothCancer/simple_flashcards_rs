337629391ee868700a6e913956cf6213eb939c79
Crax97 <gsolimeno97@gmail.com>
2023-07-29 21:16:43 +0200

    RepetitionStrategy: add tests with example usage

--- src/deck/repetition_strategy.rs
+++ src/deck/repetition_strategy.rs
@@ -14,8 +14,9 @@ pub trait RepetitionStrategy {
     fn inform_card_score(&mut self, card: &Card, guess_score: GuessScore);
 }
 
+#[derive(Debug, Default)]
 pub struct LinearRepetitionStrategy {
-    cards_to_repeat: Vec<Card>
+    cards_to_repeat: Vec<Card>,
 }
 
 impl RepetitionStrategy for LinearRepetitionStrategy {
@@ -29,7 +30,87 @@ impl RepetitionStrategy for LinearRepetitionStrategy {
 
     fn inform_card_score(&mut self, card: &Card, guess_score: GuessScore) {
         if guess_score == GuessScore::CouldNotRemember {
-            self.cards_to_repeat.push(card.clone()); 
+            // Insert on last position: repeat after all the other cards
+            self.cards_to_repeat
+                .insert(self.cards_to_repeat.len() - 1, card.clone());
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::deck::{Card, Deck};
+
+    use super::{LinearRepetitionStrategy, RepetitionStrategy};
+
+    #[test]
+    fn try_all_good() {
+        let cards = vec![
+            Card {
+                keyword: "Ciao".to_owned(),
+                description: "Hello".to_owned(),
+                priority_index: 0,
+                side: crate::deck::CardSide::Back,
+            },
+            Card {
+                keyword: "Buon giorno".to_owned(),
+                description: "Good morning".to_owned(),
+                priority_index: 0,
+                side: crate::deck::CardSide::Back,
+            },
+            Card {
+                keyword: "Notte".to_owned(),
+                description: "Night".to_owned(),
+                priority_index: 0,
+                side: crate::deck::CardSide::Back,
+            },
+        ];
+        let deck = Deck { cards };
+
+        let mut rep_strategy = LinearRepetitionStrategy::default();
+        rep_strategy.prepare_deck_repetition(&deck);
+
+        let _ = rep_strategy.next_card().unwrap();
+        let _ = rep_strategy.next_card().unwrap();
+        let _ = rep_strategy.next_card().unwrap();
+
+        assert_eq!(rep_strategy.next_card(), None);
+    }
+    #[test]
+    fn try_wrong_one() {
+        let cards = vec![
+            Card {
+                keyword: "Ciao".to_owned(),
+                description: "Hello".to_owned(),
+                priority_index: 0,
+                side: crate::deck::CardSide::Back,
+            },
+            Card {
+                keyword: "Buon giorno".to_owned(),
+                description: "Good morning".to_owned(),
+                priority_index: 0,
+                side: crate::deck::CardSide::Back,
+            },
+            Card {
+                keyword: "Notte".to_owned(),
+                description: "Night".to_owned(),
+                priority_index: 0,
+                side: crate::deck::CardSide::Back,
+            },
+        ];
+        let deck = Deck { cards };
+
+        let mut rep_strategy = LinearRepetitionStrategy::default();
+        rep_strategy.prepare_deck_repetition(&deck);
+
+        let _ = rep_strategy.next_card().unwrap();
+        let wronged = rep_strategy.next_card().unwrap();
+
+        // You're supposed to get the score from some kind of ui
+        rep_strategy.inform_card_score(&wronged, crate::deck::GuessScore::CouldNotRemember); // this card is going to be repeated
+
+        let _ = rep_strategy.next_card().unwrap();
+
+        assert_eq!(rep_strategy.next_card().unwrap().keyword, wronged.keyword);
     }
 }
